6843944f88771d6d1783f8fc270cbf84ddd67c65
Charles Wiredu <afrowebs@gmail.com>
2016-05-27 19:45:34 +0100

    front

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   protected 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:displayname, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:displayname, :email, :password, :password_confirmation, :remember_me, :firstname, :lastname) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :displayname, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:displayname, :email, :password, :password_confirmation, :current_password, :firstname, :lastname, :city, :country, :bio, :userphoto, :usercover) }
   end
