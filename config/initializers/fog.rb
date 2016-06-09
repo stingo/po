048def3f3de24669cc66a09394a1738dbdad055c
Charles Wiredu <afrowebs@gmail.com>
2016-06-10 00:13:38 +0100

    likes fixed

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,14 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => "AWS",
-    :aws_access_key_id      => ENV[''],
-    :aws_secret_access_key  => ENV['']
-  }
 
-  config.fog_directory  = ENV['pout16']
-  config.fog_public     = false
-  config.fog_region     = ENV['Ireland']
+       :provider               => 'AWS',
+       :aws_access_key_id      => "A",
+       :aws_secret_access_key  => "4",
+       :region                 => 'Ireland' # Change this for different AWS region. Default is 'us-east-1'
+
+  }
+  config.fog_directory  = "pout16"
 end
