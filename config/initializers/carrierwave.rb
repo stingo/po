2a3f08c03dbc4a4cfd9b47b4fa4c284490401df7
Charles Wiredu <afrowebs@gmail.com>
2016-06-23 23:06:12 +0100

    c

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,11 +18,11 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => '<AKIAIKSQ6UQ73NZRBMLQ>',            # required
-    :aws_secret_access_key  => '<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>',     # required
+    :aws_access_key_id      => 'AKIAIKSQ6UQ73NZRBMLQ',            # required
+    :aws_secret_access_key  => '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',     # required
     :region                 => 'eu-west-1'                        # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = '<pout16>'               # required
+  config.fog_directory  = 'pout16'               # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
