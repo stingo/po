94f0b4481a1f4e77211398241f8777e7c131d532
Charles Wiredu <afrowebs@gmail.com>
2016-06-23 22:58:59 +0100

    cw

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -22,7 +22,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => '<4yvTMIu3LrABjKPkslJHsUvOjmbM8R0YBVl8LDUz>',     # required
     :region                 => 'eu-west-1'                        # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = '<Preachout>'               # required
+  config.fog_directory  = '<pout16>'               # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
