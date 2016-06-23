8cf1bc5283443555036161eb48760ee6a6637c95
Charles Wiredu <afrowebs@gmail.com>
2016-06-23 22:50:06 +0100

    cw

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,29 @@
+# This file is not created by default so you might have to create it yourself.
+
+CarrierWave.configure do |config|
+
+  # Use local storage if in development or test
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
+  end
+
+  # Use AWS storage if in production
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+
+  config.fog_credentials = {
+    :provider               => 'AWS',                             # required
+    :aws_access_key_id      => '<AKIAIKSQ6UQ73NZRBMLQ>',            # required
+    :aws_secret_access_key  => '<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>',     # required
+    :region                 => 'eu-west-1'                        # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = '<Preachout>'               # required
+  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+  #config.fog_public     = false                                  # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
