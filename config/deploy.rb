02200d758e9034dcd59d423a63c6284461d0e9b9
Charles Wiredu <afrowebs@gmail.com>
2016-06-23 21:26:04 +0100

    dp

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,20 +42,19 @@ set :keep_releases, 2
 set :deploy_via, :remote_cache
 
 
-#set :passenger_restart_with_touch, true
+set :passenger_restart_with_touch, true
 
 
-#namespace :deploy do
+namespace :deploy do
 
-    #desc 'Restart application'
-  #task :restart do
-    #on roles(:app), in: :sequence, wait: 5 do
-     # execute :touch, release_path.join('tmp/restart.txt')
-    #end
-  #end
+    desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
 
-  after :publishing, 'deploy:restart'
+  #after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 
-end
 end
