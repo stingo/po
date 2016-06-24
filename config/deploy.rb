73002941760a011f64fd4f28d424ec15495577f4
Charles Wiredu <afrowebs@gmail.com>
2016-06-24 04:41:12 +0100

    c

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :repo_url, 'git@github.com:stingo/po.git'
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
