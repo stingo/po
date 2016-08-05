febf4a1108efc5c44abca84cd06cd5ac7565ea07
Charles Wiredu <afrowebs@gmail.com>
2016-08-05 01:21:20 +0100

    m

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAJEOWO72QKUN6YZCA",
-      :aws_secret_access_key  => "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",
+      :aws_access_key_id      => "xx",
+      :aws_secret_access_key  => "xx",
       :host                   => "s3-eu-west-1.amazonaws.com",
       :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
   }
-  config.fog_directory  = "pout16"
-end
+  config.fog_directory  = "xx"
+endxx
 
