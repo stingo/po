ca4579b7922a1d21ee0cb2c9e57d1f350938ff5e
Charlie Kay <gospelplayco@gmail.com>
2016-08-08 14:51:00 +0100

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,2 @@
 
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAJEOWO72QKUN6YZCA",
-      :aws_secret_access_key  => "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",
-      :host                   => "s3-eu-west-1.amazonaws.com",
-      :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = "pout16"
-end
-
+xxx
