21bdadb235e19252d3d97b47ea25f064b75837cd
Charles Wiredu <afrowebs@gmail.com>
2016-08-12 23:30:21 +0100

    clean

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,2 +1,13 @@
 
-xxx
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'xxx', 
+    :aws_access_key_id      => 'xxx', 
+    :aws_secret_access_key  => 'xxx', 
+    #:region                 => 'us-east-1'
+  }
+  config.fog_directory  = 'xxx'
+  config.fog_public     = false 
+  config.fog_attributes = {'xxx'}
+  config.storage = :fog
+end
