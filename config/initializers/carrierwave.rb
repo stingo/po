79278065d5e3035aad37581d0d6bfbe96c2fbfb2
Charles Wiredu <afrowebs@gmail.com>
2016-08-15 05:26:43 +0100

    clean2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,2 +1,12 @@
-
-xxx
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS', 
+    :aws_access_key_id      => 'AKIAIYWRBWFLUKbbYKUA', 
+    :aws_secret_access_key  => '+hBqW1mDdBeukdlkLg6P6e8v83lNOvVikbFWgOUr', 
+    #:region                 => 'us-east-1'
+  }
+  config.fog_directory  = 'preachersput'
+  config.fog_public     = false 
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+  config.storage = :fog
+end
